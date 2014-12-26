1e81e42ab2ed4ad49a7336375ef3495c17846fdf
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-12-26 22:48:53 +0000

    Added time functionality

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,2 +1,31 @@
+require 'twilio-ruby'
+
 class Customers
+
+  attr_accessor :name, :number
+  attr_reader :time
+
+  def initialize(name="Richard", number="447811042074")
+    @name = name
+    @number = number
+  end
+
+  def confirm_order
+    @account_sid = 'AC997198c24c36ddbe2318cb4b9fa906e8'
+    @auth_token = 'efa606d6956fc635d059c11bf1a0a411'
+    @client = Twilio::REST::Client.new @account_sid, @auth_token
+    @client.messages.create(
+    from: '441332650274',
+    to: @number,
+    body: "Thank you #{@name} for your order. It will be delivered before#{get_delivery_time}"
+    )
+    true
+  end
+
+  def get_delivery_time
+    @time_hour = Time.new.strftime("%H").to_i+1
+    @time_minute = Time.new.strftime("%M")
+    @time_now = "#{@time_hour}:#{@time_minute}"
+  end
+
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -4,7 +4,36 @@ describe Customers do
 
   let(:customers){Customers.new}
 
-  it 'send Twillio Text' do
+  context "On initialization" do
+
+    it 'should have a name' do
+    expect(customers.name).to eq("Richard")
+    end
+
+    it 'should have a default number' do
+    expect(customers.number).to eq("447811042074")
+    end
+
+
+  context "When placing an order" do
+
+    xit "can send a text" do
+      expect(customers.confirm_order).to eq(true)
+    end
+
+    it 'should be able to take a new name' do
+      expect(customers.name="Sarah").to eq("Sarah")
+    end
+
+    it 'should be able to take a new number' do
+      expect(customers.number=12345).to eq(12345)
+    end
+
+    it 'should be able to calculate the delivery time' do
+      expect(customers.get_delivery_time).to eq("#{Time.new.strftime("%H").to_i+1}:#{Time.new.strftime("%M")}")
+    end
+
   end
+end
 
 end
